5bfabb4047e03e80e96b0e779b5c382447377caf
clutak <90040861+clutak@users.noreply.github.com>
2021-11-03 14:34:50 +0100

    update on hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 name = input("What is your name? ").strip().title()
-output = "Hello {}".format(name)
 
 if not name == "":
-    print(output)
+    print("Hello {}".format(name))
 else:
     print("Hello World")
